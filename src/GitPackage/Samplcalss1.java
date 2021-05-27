fe4b8c6a88d04dbff099e1b13a59cc8245214a55
Admin <Admin@DESKTOP-NI7DS66>
2021-05-27 07:44:18 +0530

    First Commit

---
+++ src/GitPackage/Samplcalss1.java
@@ -0,0 +1,9 @@
+package GitPackage;
+
+public class Samplcalss1 {
+	public void printMethod() {
+		System.out.println("printing");
+
+	}
+
+}
---
+++ src/GitPackage/SampleClass.java
@@ -0,0 +1,10 @@
+package GitPackage;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Welcome to Git");
+
+	}
+
+}
